ef78c70480aa6d394d010ff22f8f7ec0c472a2e4
Edmond <kisemidorima37@gmail.com>
2019-11-09 10:21:36 +0800

    UI for Customer Account

--- src/main/java/controller/POSCustomerAccount.java
+++ src/main/java/controller/POSCustomerAccount.java
@@ -1,4 +1,83 @@
 package main.java.controller;
 
-public class POSCustomerAccount {
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTreeTableView;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TreeTableColumn;
+import javafx.scene.layout.StackPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class POSCustomerAccount implements Initializable {
+
+    @FXML
+    private StackPane rootPane;
+
+    @FXML
+    private JFXButton btnHome;
+
+    @FXML
+    private TextField tfSearch;
+
+    @FXML
+    private JFXButton btnSearch;
+
+    @FXML
+    private JFXButton btnCardInfo;
+
+    @FXML
+    private JFXButton btnNew;
+
+    @FXML
+    private JFXButton btnUpdate;
+
+    @FXML
+    private JFXButton btnDelete;
+
+    @FXML
+    private JFXTreeTableView<?> ttvCustomer;
+
+    @FXML
+    private TreeTableColumn<?, ?> chCustomerID;
+
+    @FXML
+    private TreeTableColumn<?, ?> chCustomerName;
+
+    @FXML
+    private TreeTableColumn<?, ?> chAddress;
+
+    @FXML
+    private TreeTableColumn<?, ?> chSex;
+
+    @FXML
+    private TreeTableColumn<?, ?> chMobileNumber;
+
+    @FXML
+    private TreeTableColumn<?, ?> chEmail;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @FXML
+    void btnHomeOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSearchOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void functionButtonOnAction(ActionEvent event) {
+
+    }
+
+
 }
