368584496dc1d6dd19f96639764b6c5562f9c799
Edmond <kisemidorima37@gmail.com>
2019-11-13 01:07:34 +0800

    Restocking option

--- src/main/java/controller/POSRestock.java
+++ src/main/java/controller/POSRestock.java
@@ -1,16 +1,23 @@
 package main.java.controller;
 
 import com.jfoenix.controls.JFXButton;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
 import main.java.misc.BackgroundProcesses;
+import main.java.misc.InputRestrictor;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 import java.util.Scanner;
 
@@ -43,6 +50,8 @@ public class POSRestock extends POSInventory{
     @FXML
     private JFXButton btnDone;
 
+    protected double price = 0;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         try {
@@ -50,13 +59,25 @@ public class POSRestock extends POSInventory{
             scan.nextLine();
             tfItemCode.setText(scan.nextLine());
             tfItemName.setText(scan.nextLine());
-            scan.nextLine();
+            price = Double.parseDouble(scan.nextLine());
             tfCurrentStock.setText(scan.nextLine());
             lblEstimatedValue.setText(scan.nextLine());
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
 
+        InputRestrictor.numbersInput(tfAddStock);
+        InputRestrictor.limitInput(tfAddStock,3);
+        Timeline clock = new Timeline(new KeyFrame(Duration.ZERO, e -> {
+            int oldVal = Integer.parseInt(tfCurrentStock.getText());
+            int newVal = tfAddStock.getText().equals("") ? 0 : Integer.parseInt(tfAddStock.getText());
+            lblNewStockValue.setText(""+(oldVal+newVal));
+            lblEstimatedValue.setText(((oldVal+newVal)*price)+"");
+        }),
+                new KeyFrame(Duration.seconds(1))
+        );
+        clock.setCycleCount(Animation.INDEFINITE);
+        clock.play();
     }
     //TODO recalculating the Text everytime the user are entering new stock value
     //TODO update database when adding stocks
