f8a81d5556051314a16e10aa27eb190d01675bbe
Edmond <kisemidorima37@gmail.com>
2019-11-15 21:52:12 +0800

    Adding some documentation in on how updating are being processed

--- src/main/java/controller/POSItemEdit.java
+++ src/main/java/controller/POSItemEdit.java
@@ -42,15 +42,22 @@ public class POSItemEdit extends POSInventory{
 
 
     private String oldCode = "";
+    private String oldName = "";
+    private String oldPrice = "";
+    private int itemId = 0;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         try {
+            /* To get the selected data from table, it is written to a text file 'cache-selected-item.file'*/
             Scanner scan = new Scanner(new FileInputStream(BackgroundProcesses.getFile("etc\\cache-selected-item.file")));
-            lblItemID.setText("Item ID : "+scan.nextLine());
-            oldCode = scan.nextLine();
+            itemId = Integer.parseInt(scan.nextLine()); // Line 1 of the cache = ItemID
+            lblItemID.setText("Item ID : "+itemId);
+            oldCode = scan.nextLine();                  // Line 2 of the cache = Item code
             tfItemCode.setText(oldCode);
-            tfItemName.setText(scan.nextLine());
-            double price = Double.parseDouble(scan.nextLine());
+            oldName = scan.nextLine();                  // Line 3 of the cache = Item name
+            tfItemName.setText(oldName);
+            oldPrice = scan.nextLine();                 // Line 4 of the cache = Item price
+            double price = Double.parseDouble(oldPrice);// Parsing the price to enable mathematical computation
             tfPrice.setText(price+"");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -64,28 +71,57 @@ public class POSItemEdit extends POSInventory{
     @FXML
     void btnCancelOnAction(ActionEvent event) {
         sceneManipulator.closeDialog();
-
     }
 
     @FXML
     void btnSaveOnAction(ActionEvent event) {
-        boolean codeExist = !this.tfItemCode.getText().equals(oldCode) ? codeExist(this.tfItemCode.getText()) : false;
-        if (hasEmptyField()){
 
+        boolean codeExist = !this.tfItemCode.getText().equals(oldCode) ? codeExist(this.tfItemCode.getText()) : false;
+
+        /*Handling Common Errors are processed within this conditions.
+        * 1 - nothing changed
+        * 2 - textfields are empty
+        * 3 - code already exist in the table
+        * 4 - code is invalid
+        * */
+
+
+        /*
+         * SCENARIO 1: If the user pressed the save button without editing any of the textfields
+         *          Simply close the dialog form for editing
+         */
+        if (nothingHasChanged()){
+            sceneManipulator.closeDialog(); // Closing the dialog if nothing has changed
+
+        /*
+         * SCENARIO 2: If there is an empty fields display an error message
+         *           that shows Invalid value of the text fields
+         */
+        }else if (hasEmptyField()){
             //An Error Message if the some of the textboxes are Empty
             POSMessage.showMessage(rootPane
                     ,"Please fill all the Fields"
                     ,"Invalid Value"
                     , POSMessage.MessageType.ERROR);
 
+        /*
+        * SCENARIO 3 : Excluding its own item code, if the Item code that is entered
+        *            are same with one of the products from table. Display an Error message
+        *            that tells the user that the item already exist
+        * */
         }else if (codeExist){
-
             //An Error Message of the Item code that is entered is already exist
             POSMessage.showMessage(rootPane
                     , "Item Code Already Exist"
                     , "Item Code Error"
                     , POSMessage.MessageType.ERROR);
 
+        /*
+        * SCENARIO 4 : If the user entered the Wrong Item code. for the
+        *              standard item code its consists of 12 or 13 digit,
+        *              if the user entered below the standard it will return
+        *              an error message
+        * */
         }else if (tfItemCode.getText().length()<12){
 
             //An Error Message if the Item code entered is less than the digit of standard bar code
@@ -94,19 +130,25 @@ public class POSItemEdit extends POSInventory{
                     , "Invalid Code"
                     , POSMessage.MessageType.ERROR);
 
+
+        /*
+        * Finally, the real update procedure will be processed here
+        * */
         }else{
             JFXButton btnNo = new JFXButton("No");
             btnNo.setOnAction(e->POSMessage.closeMessage());
 
             JFXButton btnYes = new JFXButton("Yes");
             btnYes.setOnAction(e->{
+
                 POSMessage.closeMessage();
+
                 //This is where the update will process
                 String sql = "Update Item set " +
                         "itemName = '"+tfItemName.getText()+"', " +
                         "itemCode = '"+tfItemCode.getText()+"'," +
                         "itemPrice = "+tfPrice.getText()+"" +
-                        " where itemID = "+lblItemID.getText().split(" : ")[1];//to Get the ID from the ItemID label
+                        " where itemID = "+itemId;//Basis for the update is the Id of the selected item
 
                 misc.dbHandler.startConnection();
                 misc.dbHandler.execUpdate(sql);
@@ -121,7 +163,7 @@ public class POSItemEdit extends POSInventory{
                 });
 
                 POSMessage.showConfirmationMessage(rootPane,
-                        "Item "+lblItemID.getText().split(" : ")[1]+" is now updated",
+                        "Item "+itemId+" is now updated",
                         "Update Success",
                         POSMessage.MessageType.INFORM,btnOk);
 
@@ -129,9 +171,6 @@ public class POSItemEdit extends POSInventory{
 
             POSMessage.showConfirmationMessage(rootPane,"Do you really want to update the item?"
                     ,"Please Confirm Update", POSMessage.MessageType.CONFIRM,btnNo,btnYes);
-
-
-
 
         }
 
@@ -153,6 +192,12 @@ public class POSItemEdit extends POSInventory{
             }
         }
         return itemExist;
+    }
+
+    private boolean nothingHasChanged(){
+        return tfPrice.getText().equals(oldPrice)
+                && tfItemCode.getText().equals(oldCode)
+                && tfItemName.getText().equals(oldName);
     }
 
 }
