981bd5cf40e6a1d93d080925385468f9ad42575d
Edmond <kisemidorima37@gmail.com>
2019-12-03 10:24:28 +0800

    Restricting Input in update customer UI

--- src/main/java/controller/POSCustomerEdit.java
+++ src/main/java/controller/POSCustomerEdit.java
@@ -9,7 +9,9 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.StackPane;
+import main.java.controller.message.POSMessage;
 import main.java.misc.BackgroundProcesses;
+import main.java.misc.InputRestrictor;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -67,6 +69,9 @@ public class POSCustomerEdit extends POSCustomerAccount{
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        InputRestrictor.limitInput(tfMobileNumber,11);
+        InputRestrictor.numbersInput(tfMobileNumber);
+        InputRestrictor.limitInput(tfMiddleInitial,3);
         try {
             Scanner scan = new Scanner(new FileInputStream(BackgroundProcesses.getFile("etc\\cache-selected-customer.file")));
             lblCustomerID.setText("ID : "+scan.nextLine());
@@ -93,12 +98,41 @@ public class POSCustomerEdit extends POSCustomerAccount{
 
     @FXML
     void btnSaveOnAction(ActionEvent event) {
+        if (hasEmptyField()){
+            POSMessage.showMessage(rootPane,"Please fill all the required fields","Invalid Value", POSMessage.MessageType.ERROR);
+        }else if (!emailIsValid()){
+            POSMessage.showMessage(rootPane,"The email you've entered is invalid","Invalid Value", POSMessage.MessageType.ERROR);
+        }else if (!mobileIsValid()){
+            POSMessage.showMessage(rootPane,"The mobile number you've entered is invalid","Invalid Value", POSMessage.MessageType.ERROR);
+        }else{
+            //TODO Updating data falls here
+        }
 
     }
 
     @FXML
     void sexOptionOnAction(ActionEvent event) {
 
+    }
+
+
+    private boolean hasEmptyField(){
+        return tfFirstName.getText().equals("") ||
+                tfLastName.getText().equals("") ||
+                tfMobileNumber.getText().equals("") ||
+                tfAddress.getText().equals("") ||
+                !(rbFemale.isSelected() || rbMale.isSelected());
+    }
+
+    private boolean mobileIsValid(){
+        return (tfMobileNumber.getText().startsWith("0") && tfMobileNumber.getText().length() == 11) ||
+                (tfMobileNumber.getText().startsWith("9") && tfMobileNumber.getText().length() ==10);
+
+    }
+
+    private boolean emailIsValid(){
+        return tfEmailAddress.getText().contains("@")
+                && tfEmailAddress.getText().contains(".");
     }
 
 }
